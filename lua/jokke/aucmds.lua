7986c4db95efcc670eda3bddd51b29e1ab024e1d
Joachim Mero <joachim.mero@nextgames.com>
2024-02-05 20:26:25 +0200

    Add more omnisharp keymaps

--- lua/jokke/aucmds.lua
+++ lua/jokke/aucmds.lua
@@ -8,5 +8,7 @@ autocmd('Filetype', {
   pattern = { 'cs' },
   callback = function()
     vim.keymap.set('n', '<leader>gd', '<Plug>(omnisharp_go_to_definition)', {silent = true, buffer = true})
+    vim.keymap.set('n', '<leader>fu', '<Plug>(omnisharp_find_usages)', {silent = true, buffer = true})
+    vim.keymap.set('n', '<leader>fi', '<Plug>(omnisharp_find_implementations)', {silent = true, buffer = true})
   end
 })
