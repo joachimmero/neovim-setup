17f37d1746c91d3c7c4c9e96fc76022d0a8a6b76
Joachim Mero <joachim.mero@nextgames.com>
2024-02-05 10:43:50 +0200

    Explicitly use .NET 6

--- lua/jokke/plug.lua
+++ lua/jokke/plug.lua
@@ -16,6 +16,7 @@ vim.cmd('filetype plugin indent on')
 vim.cmd('syntax enable')
 
 vim.g.OmniSharp_server_use_mono = 1
+vim.g.OmniSharp_server_use_net6 = 1
 
 vim.g.ale_linters = {
 cs = {'OmniSharp'}
